d5394ae2c5931bc83c8a5dbaf60cc87a19801080
Artur Borkowski <gdx149021@student.wsb.gda.pl>
2022-06-25 20:03:52 +0200

    Dodaj pliki projektów.

---
+++ dot_net_lab4_mvc/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using dot_net_lab4_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dot_net_lab4_mvc.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            HelloModel model = new();
+            model.Imie = "Artur";
+            model.Nazwisko = "Borkowski";
+            model.Indeks = "57647";
+
+            return View(model);
+        }
+    }
+}
---
+++ dot_net_lab4_mvc/Models/HelloModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace dot_net_lab4_mvc.Models
+{
+    public class HelloModel
+    {
+        [DisplayName("Imię studenta")]
+        public string? Imie { get; set; }
+        
+        [DisplayName("Nazwisko studenta")]
+        public string? Nazwisko { get; set; }
+        
+        [DisplayName("Numer indeksu")]
+        public string? Indeks { get; set; }
+
+    }
+}
